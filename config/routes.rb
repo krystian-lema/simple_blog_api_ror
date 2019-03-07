c12bdca5fbed85175cad1790477bc1e23dcaeb81
Krystian Lema <krystian.lema@gmail.com>
2019-03-07 09:33:28 +0100

    Fix route for include all data method in articles controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :tags
   resources :comments
-  resources :articles
-  get '/articles_include_all_data', to: 'articles#include_all_data'
+  resources :articles do
+    collection do
+      get :include_all_data
+    end
+  end
   resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
